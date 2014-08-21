144da0a6f86ea365580cdd659d77bd5b1c03f10c
Preto <leander@true.nl>
2014-08-21 15:40:14 +0200

    Forgot again that bash doesn't handle floats :-)

--- mysql/mysql-max-connections-tracker.sh
+++ mysql/mysql-max-connections-tracker.sh
@@ -6,7 +6,7 @@
 MAX_PERCENT="80"
 OUTFILE="/var/log/mysql/mysql-max-connections-tracker.log"
 
-PERCENT=$((/usr/bin/mysql --defaults-extra-file=/etc/mysql/debian.cnf --skip-column-names -Be "select ( pl.connections / gv.max_connections ) * 100 as percentage_used_connections from ( select count(*) as connections from information_schema.processlist ) as pl, ( select VARIABLE_VALUE as max_connections from information_schema.global_variables where variable_name = 'MAX_CONNECTIONS' ) as gv;") 2>&1)
+PERCENT=$((/usr/bin/mysql --defaults-extra-file=/etc/mysql/debian.cnf --skip-column-names -Be "select ( pl.connections / gv.max_connections ) * 100 as percentage_used_connections from ( select count(*) as connections from information_schema.processlist ) as pl, ( select VARIABLE_VALUE as max_connections from information_schema.global_variables where variable_name = 'MAX_CONNECTIONS' ) as gv;") | awk '{printf "%0.0f", $0}' 2>&1)
 
 if [[ "${PERCENT}" > "${MAX_PERCENT}" ]]; then
 	/usr/bin/logger -p user.crit "Max MySQL connections almost reached"

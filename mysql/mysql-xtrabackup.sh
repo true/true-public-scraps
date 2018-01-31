56a892f1e72a62f8a1269d684f671903f872b4ba
true-patrick <patrick@true.nl>
2018-01-31 15:27:18 +0100

    Update mysql-xtrabackup.sh

--- mysql/mysql-xtrabackup.sh
+++ mysql/mysql-xtrabackup.sh
@@ -2,21 +2,26 @@
 #
 # True MySQL backup script for Xtrabackup implementations
 #
-# Author   : Rene Bakkum
-# Version  : 1.01
+# Author   : Rene Bakkum & Patrick Lina
+# Version  : 2.0.0
 # Copyright: True B.V.
-#            In case you would like to make changes, let me know!
+#            In case you would like to make changes, let Rene know!
 #
 # Changelog:
 # 1.00 Initial release
 # 1.01 Added external config
+# 2.0.0 Major rewrite
+#set -x
 
-PID="/backup/xtraback-running.lock"
-MAILTO="root"
+SELF="$(basename $0)"
+
+LOCK="/backup/xtrabackup-running.lock"
+LOG="/backup/xtrabackup-running.log"
+MAILTO=""
 MAILFROM="$(hostname)@$(hostname -d)"
 BACKUPPATH="/backup/xtrabackup"
-SUBJECT="[MySQL] Backup is still running"
 # Retention periods (in minutes)
+KEEP_CLEANUP="pre"
 KEEP_UNCOMPRESSED="180"
 KEEP_COMPRESSED="2880"
 
@@ -24,10 +29,13 @@ CFG="${0/.sh/.cfg}"
 
 if [ ! -e "$CFG" ]; then
   cat << EOF > "$CFG"
+LOCK="$LOCK"
+LOG="$LOG"
 MAILTO="$MAILTO"
+MAILFROM="$MAILFROM"
 BACKUPPATH="$BACKUPPATH"
-SUBJECT="$SUBJECT"
 # Retention periods (in minutes)
+KEEP_CLEANUP="$KEEP_CLEANUP"
 KEEP_UNCOMPRESSED="$KEEP_UNCOMPRESSED"
 KEEP_COMPRESSED="$KEEP_COMPRESSED"
 EOF
@@ -35,75 +43,75 @@ else
   source "$CFG"
 fi
 
-if [ ! -e "$PID" ]; then
-  echo "Backup is still running, starting date: $(date)" > "$PID"
-  ####
-  # Let's make the backup!
-  ####
-  time innobackupex --default-extra-file=/etc/mysql/debian.cnf $BACKUPPATH
+## Capture all output
+exec 5>&1 6>&2 1>"$LOG" 2>&1
 
-  s=$?
-  if [ $s -ne 0 ]; then
-    STATUS="FAIL";
-    SUBJECT="Xtrabackup step failed."
-    /usr/bin/mailx -r $MAILFROM -s "[$STATUS] $SUBJECT" $MAILTO < /dev/null
+## Send email and exit
+## Usage: mail_and_exit [<exitcode> ["<subject>" ["<contentfile>"]]]
+function mail_and_exit {
+  exitcode="${1:-0}"
+  subject="${2:-$SELF}"
+  content="${4:-$LOG}"
+
+  ## Check if sender and recipient are configured
+  if [ -n "$MAILFROM" -a -n "$MAILTO" ]; then
+    ## Send email with content
+    /usr/bin/mailx -r "$MAILFROM" -s "$subject" "$MAILTO" < "$content"
+  else
+    ## Undo logging and show content
+    exec 1>&5 2>&6
+    echo "***" >&2
+    echo "*** $subject" >&2
+    echo "***" >&2
+    cat "$content" >&2
   fi
 
-  latestdir="$(ls -t $BACKUPPATH | head -1)"
-  ####
-  # Apply the logs.
-  ####
-  time innobackupex --apply-log --use-memory=4G $BACKUPPATH/$latestdir
+  ## Exit program
+  exit "$exitcode"
+}
 
-  s=$?
-  if [ $s -ne 0 ]; then
-    STATUS="FAIL";
-    SUBJECT="Xtrabackup APPLY step failed."
-    /usr/bin/mailx -r $MAILFROM -s "[$STATUS] $SUBJECT" $MAILTO < /dev/null
-  fi
+# Remove the lock file so new backup will start
+function clean_exit {
+  rm "$LOCK"
+}
 
-  ####
-  # Since we want multicore compression, we are going to make 1 big tar file before pigz kicks in..
-  ####
-  time tar -cf $BACKUPPATH/$latestdir.tar $BACKUPPATH/$latestdir
-
-  s=$?
-  if [ $s -ne 0 ]; then
-    STATUS="FAIL";
-    SUBJECT="Tar step failed."
-    /usr/bin/mailx -r $MAILFROM -s "[$STATUS] $SUBJECT" $MAILTO < /dev/null
-  fi
-
-  ####
-  # Let's compress it... you need pigz installed (apt install pigz)
-  ####
-  time pigz --fast $BACKUPPATH/$latestdir.tar
-
-  s=$?
-  if [ $s -ne 0 ]; then
-    STATUS="FAIL";
-    SUBJECT="Pigz step failed."
-    /usr/bin/mailx -r $MAILFROM -s "[$STATUS] $SUBJECT" $MAILTO < /dev/null
-  fi
-
-  ####
+function cleanup {
   # Remove uncompressed backups older than xx minutes...
-  ####
   find "$BACKUPPATH" -maxdepth 1 ! -path "$BACKUPPATH" -type d -mmin +$KEEP_UNCOMPRESSED -exec rm -Rf {} \;
 
-  ####
   # Remove compressed backups older than xx minutes...
-  ####
-  find "$BACKUPPATH" -type f -mmin +$KEEP_COMPRESSED -delete
+  find "$BACKUPPATH" -type f -name "*.tgz" -mmin +$KEEP_COMPRESSED -delete
+}
 
-  ####
-  # Remove the lock file so new backup will start
-  ####
-  rm $PID
+###
+### Main program
+###
+if [ -e "$LOCK" ]; then
+  # Report that the backup is still running, instead of starting multiple backups
+  mail_and_exit 1 "[MySQL] Backup is still running" "$LOCK"
 
 else
-  ####
-  # Report that the backup is still running, instead of starting multiple backups
-  ####
-  /usr/bin/mailx -r $MAILFROM -s "$SUBJECT" $MAILTO < $PID
+
+  ## Set lock file and clean it up on exit
+  echo "Backup is still running, starting date: $(date)" > "$LOCK"
+  trap clean_exit exit
+
+  ## Pre cleanup
+  [ "$KEEP_CLEANUP" == "pre" ] && cleanup
+
+  # Let's make the backup!
+  time innobackupex --default-extra-file=/etc/mysql/debian.cnf "$BACKUPPATH"
+  [ $? -ne 0 ] && mail_and_exit 1 "[FAIL] Xtrabackup step failed."
+
+  # Apply the logs.
+  latestdir="$(ls -t "$BACKUPPATH" | head -1)"
+  time innobackupex --apply-log --use-memory=4G "${BACKUPPATH}/${latestdir}"
+  [ $? -ne 0 ] && mail_and_exit 1 "[FAIL] Xtrabackup APPLY step failed."
+
+  # Since we want multicore compression, we are going to make 1 big tar file before pigz kicks in..
+  time tar -cf - "${BACKUPPATH}/${latestdir}" | pigz --fast >"${BACKUPPATH}/${latestdir}.tgz"
+  [[ "${PIPESTATUS[*]}" =~ [^0\ ] ]] && mail_and_exit 1 "[FAIL] Compression step failed."
+
+  ## Post cleanup
+  [ "$KEEP_CLEANUP" != "pre" ] && cleanup
 fi
